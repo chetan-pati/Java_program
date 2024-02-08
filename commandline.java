fc20d6f1db4d412363e4316e295480a8c3fec902
chetan-pati <chetanpatil5857@gmail.com>
2024-02-08 20:28:43 +0530

    [ADDED] : commandline program

---
+++ commandline.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class commandline
+{
+	public static void main(string args[])
+	{
+	 System.out.println("First argument is:"+args[0]);
+	 System.out.println("Second argument is:"+args[1]);
+
+ }
+}
